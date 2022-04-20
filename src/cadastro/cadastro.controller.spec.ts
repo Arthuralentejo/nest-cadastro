6343a188cf8be8b4ca6ee333c1ff9f9ccfe586d6
Arthuralentejo <arthur.alentejo228@gmail.com>
2022-04-20 12:05:45 -0300

    Configuring prettier and eslint

--- src/cadastro/cadastro.controller.spec.ts
+++ src/cadastro/cadastro.controller.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { CadastroController } from "./cadastro.controller";
+import { Test, TestingModule } from '@nestjs/testing';
+import { CadastroController } from './cadastro.controller';
 
-describe("CadastroController", () => {
+describe('CadastroController', () => {
   let controller: CadastroController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [CadastroController]
+      controllers: [CadastroController],
     }).compile();
 
     controller = module.get<CadastroController>(CadastroController);
   });
 
-  it("should be defined", () => {
+  it"should be defined"', () => {
     expect(controller).toBeDefined();
   });
 });
--- src/cadastro/cadastro.controller.ts
+++ src/cadastro/cadastro.controller.ts
@@ -1,5 +1,4 @@
-import { Controller } from "@nestjs/common";
+import { Controller } from '@nestjs/common';
 
-@Controller("cadastro")
-export class CadastroController {
-}
+@Controller('cadastro')
+export class CadastroController {}
